9268762956d6fc2ed36d70fdd58b97916cbc3547
kavishkanimsara <nimsaragk.21@uom.lk>
2025-02-03 12:31:16 +0530

    39 - URL Query Parameters

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,7 +1,24 @@
+import { NextRequest } from "next/server";
 import { reviewData } from "../comments/data";
 
-export async function GET(){
-    return Response.json(reviewData);
+// normal get all operation
+
+// export async function GET(){
+//     return Response.json(reviewData);
+// }
+
+// get with query parameters
+export async function GET(request : NextRequest){
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+    if(query){
+        const filteredData = reviewData.filter((review) => review.review.includes(query));
+        return new Response(JSON.stringify(filteredData), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        });
+    }
 }
 
 
@@ -16,5 +33,5 @@ export async function POST(request : Request){
 
     reviewData.push(newReview)
     return new Response("Review added successfully !")
-
 }
+
